1614a9bc57e5df353a1ed388fc444bba0ea285ec
Mg Chit <chitlunh@gmail.com>
2024-01-17 03:24:26 +0630

    -

--- DotNet8WebApi.LiteDbSample/Program.cs
+++ DotNet8WebApi.LiteDbSample/Program.cs
@@ -1,4 +1,5 @@
 using DotNet8WebApi.LiteDbSample.Services;
+using LiteDB;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -8,8 +9,15 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
-builder.Services.AddScoped<LiteDbV2Service>();
+builder.Services.AddScoped(n =>
+{
+    var _folderPath = Path.Combine(AppDomain.CurrentDomain.BaseDirectory, "LiteDb");
+    Directory.CreateDirectory(_folderPath);
 
+    var _filePath = Path.Combine(_folderPath, "Blog.db");
+    return new LiteDatabase(_filePath);
+});
+builder.Services.AddScoped<LiteDbV2Service>();
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
--- DotNet8WebApi.LiteDbSample/Services/LiteDbV2Service.cs
+++ DotNet8WebApi.LiteDbSample/Services/LiteDbV2Service.cs
@@ -5,16 +5,22 @@ namespace DotNet8WebApi.LiteDbSample.Services
 {
     public class LiteDbV2Service:IDisposable
     {
-        private readonly LiteDatabase _liteDatabase;
-        private readonly string _filePath;
-        private readonly string _folderPath;
-        public LiteDbV2Service()
-        {
-            _folderPath = Path.Combine(AppDomain.CurrentDomain.BaseDirectory, "LiteDb");
-            Directory.CreateDirectory(_folderPath);
+        //private readonly LiteDatabase _liteDatabase;
+        //private readonly string _filePath;
+        //private readonly string _folderPath;
+        //public LiteDbV2Service()
+        //{
+        //    _folderPath = Path.Combine(AppDomain.CurrentDomain.BaseDirectory, "LiteDb");
+        //    Directory.CreateDirectory(_folderPath);
 
-            _filePath = Path.Combine(_folderPath, "Blog.db");
-            _liteDatabase = new LiteDatabase(_filePath);
+        //    _filePath = Path.Combine(_folderPath, "Blog.db");
+        //    _liteDatabase = new LiteDatabase(_filePath);
+        //}
+        private readonly LiteDatabase _liteDatabase;
+
+        public LiteDbV2Service(LiteDatabase liteDatabase)
+        {
+            _liteDatabase = liteDatabase;
         }
 
         public ILiteCollection<BlogModel> Blog => _liteDatabase.GetCollection<BlogModel>("Blog");
