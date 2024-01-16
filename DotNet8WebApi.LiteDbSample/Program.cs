1aa10d9443d7c7fbe8a4bb37403c555508827fbf
Mg Chit <chitlunh@gmail.com>
2024-01-17 03:04:59 +0630

    -

--- DotNet8WebApi.LiteDbSample/Program.cs
+++ DotNet8WebApi.LiteDbSample/Program.cs
@@ -1,3 +1,5 @@
+using DotNet8WebApi.LiteDbSample.Services;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -6,6 +8,7 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+builder.Services.AddScoped<LiteDbService>();
 
 var app = builder.Build();
 
--- DotNet8WebApi.LiteDbSample/Services/LiteDbService.cs
+++ DotNet8WebApi.LiteDbSample/Services/LiteDbService.cs
@@ -1,7 +1,24 @@
-﻿namespace DotNet8WebApi.LiteDbSample.Services
+﻿using DotNet8WebApi.LiteDbSample.Models;
+using LiteDB;
+
+namespace DotNet8WebApi.LiteDbSample.Services
 {
     public class LiteDbService
     {
+        private readonly LiteDatabase _liteDatabase;
+        private readonly string _filePath;
+        private readonly string _folderPath;
+        public LiteDbService()
+        {
+            _folderPath = Path.Combine(AppDomain.CurrentDomain.BaseDirectory, "LiteDb");
+            Directory.CreateDirectory(_folderPath);
 
+            _filePath = Path.Combine(_folderPath, "Blog.db");
+            _liteDatabase = new LiteDatabase(_filePath);
+        }
+
+        public ILiteCollection<BlogModel> Blog => _liteDatabase.GetCollection<BlogModel>("Blog");
+
+        public void Dispose() => _liteDatabase.Dispose();
     }
 }
