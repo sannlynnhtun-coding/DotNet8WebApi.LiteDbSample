e3246a2fece160ddc52cd21c41807650302d6248
OrdinaryMack <mackk5504@gmail.com>
2024-01-18 03:26:48 +0630

    [Add]RequestModel

--- DotNet8WebApi.LiteDbSample/Models/Blog/BlogRequestModel.cs
+++ DotNet8WebApi.LiteDbSample/Models/Blog/BlogRequestModel.cs
@@ -3,5 +3,7 @@
     public class BlogRequestModel
     {
         public string? BlogTitle { get; set; }
+        public string? BlogAuthor { get; set; }
+        public string? BlogContent { get; set; }
     }
 }
